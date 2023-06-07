636997f0d6cac3ca16ffaa5711d368c2e74401d4
garra400 <118711201+garra400@users.noreply.github.com>
2023-06-07 15:52:24 -0300

    ADD Compra and CompraProduto

--- Projeto/api/compra/compra.dao.php
+++ Projeto/api/compra/compra.dao.php
@@ -23,18 +23,39 @@ class CompraDAO {
      */
     public function insert($compra) {
         $stmt = $this->pdo->prepare("INSERT INTO tb_compra
-            (id_usuario) 
-            VALUES (:id_usuario)");
+            (id_usuario, data_compra) 
+            VALUES (:id_usuario, :data_compra)");
         
         $stmt->bindValue("id_usuario", $compra->id_usuario);
+        $stmt->bindValue("data_compra", $compra->data_compra);
 
         $stmt->execute();
         $compra = clone $compra;
         $compra->id = $this->pdo->lastInsertId();
+
+        foreach ($compra->produtos as $produto) {
+            $stmt = $this->pdo->prepare("INSERT INTO tb_compra_produto
+            (id_compra, id_produto, preco_produto, quantidade) 
+            VALUES (:id_compra, :id_produto, :preco_produto, :quantidade)");
+        
+            $stmt->bindValue("id_compra", $compra->id);
+            $stmt->bindValue("id_produto", $produto->id);
+            $stmt->bindValue("preco_produto", $produto->preco);
+            $stmt->bindValue("quantidade", $produto->quantidade);
+
+            $stmt->execute();
+        }
         return $compra;
     }
 
     public function delete($id) {
+        $stmt = $this->pdo->prepare("DELETE FROM tb_compra_produto
+        WHERE id_compra=:id_compra");
+        
+        $stmt->bindValue("id_compra", $id);
+
+        $stmt->execute();
+        
         $stmt = $this->pdo->prepare("DELETE FROM tb_compra
         WHERE id=:id");
         
@@ -48,14 +69,15 @@ class CompraDAO {
 
     public function update($id, $compra) {
         $stmt = $this->pdo->prepare("UPDATE tb_compra SET
-                id_usuario = :id_usuario
+                id_usuario = :id_usuario, data_compra = :data_compra
                 WHERE id = :id");
         
         $data = [
             "id" => $id,
             "id_usuario" => $compra->id_usuario,
+            "data_compra" => $compra->data_compra
         ];
-
+        print_r($data);
         return $stmt->execute($data);
     }
 }
